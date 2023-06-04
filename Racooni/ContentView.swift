ef4472b3dfd627936c94c18bcbe9b12407fe5318
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2023-06-04 18:04:26 -0400

    racooni current

--- Racooni/ContentView.swift
+++ Racooni/ContentView.swift
@@ -1,26 +1,22 @@
-//
-//  ContentView.swift
-//  Racooni
-//
-//  Created by Thomas Stubblefield on 6/4/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+
+    @ObservedObject var viewModel = ViewModel()
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            ForEach(viewModel.posts, id: \.self?.thumbnail) { post in
+                Text("test")
+            }
             Text("Hello, world!")
+                .onTapGesture {
+                    print(viewModel.posts)
+                }
         }
         .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
