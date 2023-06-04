6d02740716efd34a5709614ff6366845a0a244b3
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2023-06-04 17:03:37 -0400

    Initial Commit

---
+++ Racooni/RacooniApp.swift
@@ -0,0 +1,17 @@
+//
+//  RacooniApp.swift
+//  Racooni
+//
+//  Created by Thomas Stubblefield on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RacooniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
